522e405dc720907f1d698f44fdafd5a5f536b4d6
zylstraj <colbertlove12@gmail.com>
2017-03-06 13:52:20 -0800

    Adding Portfolio projects

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -9,6 +9,8 @@ export class Portfolio extends React.Component {
         {/* <p className={styles.portfolio}>Currently Finishing Up A Few Projects</p>*/}
          <h1 className={styles.portfolio_h1}><a href="http://no-water.surge.sh">No Water.<br></br>You Die.</a></h1>
        {/* // <p>Raise awareness about the lack of world through the world</p>*/}
+       <h1 className={styles.portfolio_h1}><a href="http://miniyoutubereact.herokuapp.com">Mini YouTube</a></h1>
+       <h1 className={styles.portfolio_h1}><a href="http://pnwweatherreact.herokuapp.com">PNW Weather</a></h1>
       </section>
   )
   }
